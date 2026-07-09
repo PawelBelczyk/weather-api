bcee07ae279f939c8c8dc8e91cc7d2047ba6035f
PawelBelczyk <227667461+PawelBelczyk@users.noreply.github.com.>
2026-07-09 15:06:01 +0200

    dom logic added

--- src/index.js
+++ src/index.js
@@ -4,8 +4,29 @@ import {getWeather} from "./weather.js"
 const weatherForm = document.getElementById("weather-form");
 const userInput = document.getElementById("user-input");
 
-weatherForm.addEventListener("submit",(e) => {
+weatherForm.addEventListener("submit", async (e) => {
     e.preventDefault();
-    getWeather(userInput.value);
+   const data = await getWeather(userInput.value);
+   console.log(data);
+   
+   const location = document.getElementById("location");
+   const temperature = document.getElementById("temperature");
+   const feelsLike = document.getElementById("feelsLike");
+   const humidity = document.getElementById("humidity");
+   const wind = document.getElementById("wind");
+   const description = document.getElementById("description");
+   const weather = document.getElementById("weather-icon");
+   
+   // weather-icon.src = `https:${data.current.condition.icon}`;
+   // weather-icon.hidden = false;
+   
+   location.textContent = data.location.name;
+   temperature.textContent = `${data.current.temp_c} °C`;
+   feelsLike.textContent = `${data.current.feelslike_c} °C`;
+   humidity.textContent = `${data.current.humidity}%`;
+   wind.textContent = `${data.current.wind_kph} km/h`;
+   description.textContent = data.current.condition.text;
+   weather.src = `https:${data.current.condition.icon}`;
+   weather.hidden = false;
     
 });
--- src/weather.js
+++ src/weather.js
@@ -1,5 +1,12 @@
 
 export async function getWeather(city) {
-    console.log(city);
-    
+
+const API = "579307d8e2fe4e8fb4582945260907"    
+
+const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API}&q=${city}`);
+const data = await response.json();
+return data;
+
 }
+
+// weather.js odpowiada tylko i wyłącznie za komunikację z api.0
